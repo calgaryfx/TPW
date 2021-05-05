62465312941bbf9cffc06314130817dec3aab014
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-05 14:52:55 -0600

    Tax and tip moved to variables. Simpler to change if needed.

--- ex_6.py
+++ ex_6.py
@@ -1,18 +1,22 @@
 # Ex.6: Tax and Tip (p.4)
 
+# Set tax and tip values.
+tax = 0.05
+tip = 0.18
+
 # Print the cost of the meal.
 order = float(input("Cost of the meal: "))
 
 # Compute tax and tip.
-tax = order * 0.05
-tip = order * 0.18
+order_tax = order * tax
+order_tip = order * tip
 
 # Grand total.
-total = order + tax + tip
+total = order + order_tax + order_tip
 
 # Receipt showing tax amount, tip amount, Grand total.
-print(f"Tax: $%.2f" % tax)
-print(f"Tip: $%.2f" % tip)
+print(f"Tax: $%.2f" % order_tax)
+print(f"Tip: $%.2f" % order_tip)
 print(f"Grand Total: $%.2f" % total)
 
 # Task: Program to read the cost of a meal ordred at a restaurant from the user.
