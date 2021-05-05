4a31ec5368fe08ea10a7d8c90d5874ab05dbe4ed
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-05 12:47:14 -0600

    First commit. Complete.

---
+++ ex_4.py
@@ -0,0 +1,13 @@
+# Ex.4: Area of a Field (p.4)
+length = float(input("Please input the length of the field in feet: "))
+width = float(input("Please input the width of the field in feet: "))
+
+# There are 43_450 feet in an acre.
+area_acres = (length * width) / 43_560
+
+print(f"The field has an area of {area_acres} acres.")
+
+# Task: Create a program that reads the length and width of a farmer's field from
+# the user in feet. Display the area of the field in acres.
+
+# The Python Workbook practice. Solution (p.88).
