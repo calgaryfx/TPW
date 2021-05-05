15f8f5886d5cb5fc67c34d3f0ae0aed5a99a295f
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-05 11:45:52 -0600

    First commit. Program complete.

---
+++ ex_1.py
@@ -0,0 +1,15 @@
+# The Python Workbook practice. Solutions start at P.87.
+# Ex.1: Mailing Address (p.3).
+recipiant = "Mark Rogers"
+address1 = "999 Xxxxx Mews. S.E."
+address2 = "Calgary, Alberta"
+postal_code = "T2X 1C3"
+
+print(recipiant)
+print(address1)
+print(address2)
+print(postal_code)
+
+# Task: Create a program that displays your name and complete mailing address in
+# the manner that you would see it on the outside of an envelope.
+# The program does not need to read input from the user.
