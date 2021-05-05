0979c24434f2c76d6d64a918cd659f6249233bc3
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-05 14:51:20 -0600

    Receipt turned into one line like Author.

---
+++ ex_6b.py
@@ -0,0 +1,26 @@
+# Ex.6: Tax and Tip (p.4)
+
+# Set tax and tip values.
+tax = 0.05
+tip = 0.18
+
+# Print the cost of the meal.
+order = float(input("Cost of the meal: "))
+
+# Compute tax and tip.
+order_tax = order * tax
+order_tip = order * tip
+
+# Grand total.
+total = order + order_tax + order_tip
+
+# Receipt showing tax amount, tip amount, Grand total.
+print(f"The meal cost $%.2f. Taxes are $%.2f. Tip is $%.2f." % (order_tax, order_tip, total))
+
+# Task: Program to read the cost of a meal ordred at a restaurant from the user.
+# Compute the tax and tip for the meal.
+# Compute the tip as 18% of the meal amount (without the tax).
+# Output: Tax amount, Tip amount and Grand total for the meal including tax and
+# tip. Format the output so all the values are displayed to two decimal places.
+
+# The Python Workbook practice. Solution (p.88).
