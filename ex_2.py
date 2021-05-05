37023e44f3222d2ccd21337d4fb271fd05c8c77c
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-05 11:43:10 -0600

    First commit. Program finished.

---
+++ ex_2.py
@@ -0,0 +1,7 @@
+# Ex.2: Hello (p.3)
+user = input("Please input your name: ")
+print(f"Hello {user.title()}.")
+
+# Task: Write a program that asks the user to enter his or her name.
+# The program should respond with a hello message to the user, using his or her name.
+# The Python Workbook practice. Solution not available.
