0e3d2a137f7b47447eb2add54dc88d93db2f22dd
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-05 11:56:09 -0600

    First commit. Complete.

---
+++ ex_3.py
@@ -0,0 +1,15 @@
+# Ex.3: Area of a Room (p.4)
+width = float(input("Please enter the width of the room in meters: "))
+length = float(input("Please enter the length of the room in meters: "))
+area = width * length
+print(f"The area of the room equals: {area}m.")
+
+
+# Task: Write a program that asks the user to enter the width and length of a room.
+# Once the values have been read, the program should compute and display the area of
+# the room.
+# The length and width will be entered as floating point numbers.
+# Include units in your prompt and output message; either feet or meters, depending
+# on which unit you are more comfortable working with.
+
+# The Python Workbook practice. Solution (p.87).
