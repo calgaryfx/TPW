6f15ac209550a0e02e2071ae21cd2582abadbee2
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-06 08:08:53 -0600

    Authors version. Print statement different.

---
+++ ex_7_author.py
@@ -0,0 +1,15 @@
+# Ex.7: Sum fo the First n Positive Integers (p.5). Author version.
+
+n = int(input("Enter a positive integer: "))
+
+# Compute the sum.
+sm = n * (n + 1) / 2
+
+# Display the result
+print("The sum of the first", n, "positive integers is", sm)
+
+# Task: Write a program that reads a positive integer, n, from the user and then
+# displays the sum of all the integers from 1 to n.
+# The sum of the first n positive integers can be computed with this formula:
+# sum = n * (n + 1) / 2
+# The Python Workbook practice. Solution (p.89).
