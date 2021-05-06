d441a1b0209eb57c866388dd9bf3171b66ea3a48
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-06 08:04:47 -0600

    First commit. Complete.

---
+++ ex_7.py
@@ -0,0 +1,14 @@
+# Ex.7: Sum fo the First n Positive Integers (p.5).
+
+# User input positive integer
+n = int(input("Input a positive number: "))
+
+# Calculate and display the sum.
+sum = n * (n + 1) / 2
+print(sum)
+
+# Task: Write a program that reads a positive integer, n, from the user and then
+# displays the sum of all the integers from 1 to n.
+# The sum of the first n positive integers can be computed with this formula:
+# sum = n * (n + 1) / 2
+# The Python Workbook practice. Solution (p.89).
