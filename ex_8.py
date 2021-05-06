34b2bd4b7f2e7034cf7fbda81fc19b3d0f5506d5
Mark Rogers <markrogers@Marks-MacBook-Air.local>
2021-05-06 08:46:13 -0600

    First commit. Complete.

---
+++ ex_8.py
@@ -0,0 +1,21 @@
+# Ex.8: Widgets and Gizmos (p.5).
+
+# Widget and Gizmos weights in grams.
+widget_weight = 75
+gizmo_weight = 112
+
+# Order.
+widgets = int(input("How many widgets do you want to order? "))
+gizmos = int(input("How many gizmos do you want to order? "))
+
+# Compute weight.
+total_weight = (widgets * widget_weight) + (gizmos * gizmo_weight)
+
+# Display weight of the order.
+print(f"The total weight of your order is {total_weight} grams.")
+
+# Task: An online retailer sells two products: Widgets and gizmos. Each Widget
+# weighs 75 grams. Each gizmo weighs 112 grams. Write a program that reads the
+# number of widgets and the number of gizmos in an order from the user. Then compute
+# and display the total weight of the order.
+# The Python Workbook practice. No solution given.
